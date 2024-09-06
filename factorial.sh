7baa7448410bce2c589958d11ce5bf48a388cd79
omarAlzubi22 <omar.alzubi@uconn.edu>
2024-09-06 11:53:48 -0400

    Update factorial.sh

--- factorial.sh
+++ factorial.sh
@@ -1,1 +1,14 @@
 
+# !/bin/bash
+
+echo "enter any number"
+read num #read number from user
+fact = 1 #factorial of 0 is 1, calculation starts from 1
+
+for ((i=1;i<=num;i++))
+do
+fact =$ (($fact*$i)) #in each iteration, current value of fact is multiplied by i and stored back into fact
+done
+
+echo $fact
+
